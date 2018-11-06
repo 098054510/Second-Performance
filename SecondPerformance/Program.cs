12bde1d38947cb0a66720dc474f48526db602948
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 07:31:33 -0300

    Second Performance/ Program made in C#

---
+++ SecondPerformance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace SecondPerformance
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int N;
+            double x, y, resultado;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i=0; i<N; i++) {
+                string[] vet = Console.ReadLine().Split(' ');
+                x = double.Parse(vet[1], CultureInfo.InvariantCulture);
+                y = double.Parse(vet[2], CultureInfo.InvariantCulture);
+                if(y == 0.0)
+                {
+                    Console.WriteLine("Não Existe");
+                }
+                else
+                {
+                    resultado = x / y;
+                    Console.WriteLine(resultado);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
